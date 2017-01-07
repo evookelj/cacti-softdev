83e1886a23995c8af3185a30ca54eac938bd58ee
evookelj <evukelj@stuy.edu>
2017-01-07 13:40:19 -0500

    Start filling in quench

---
+++ utils/quench.py
@@ -0,0 +1,6 @@
+def calcTime(tweet, hasImage):
+    # get text processing on tweet
+    # turn these results into a query string
+    # call from twitter w this query
+    # store results from these tweets
+    # weight time based on likes/retweets
