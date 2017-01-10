46f866ca66eb53bf1db115cb84fc87d3f6591e79
Yvonne Chan <ychan1352@gmail.com>
2017-01-10 12:33:27 -0500

    VERY basic app.py and began twitter handling

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, session, request, url_for, redirect, render_template
+from utils import twitter, textprocess
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "something"
+
+if __name__="__main__":
+    app.debug = True
+    app.run()
---
+++ utils/twitter.py
@@ -0,0 +1,7 @@
+import json
+
+twitter=open("api.txt","r")
+tw_key=twitter.read().strip
+twitter.close()
+
+
