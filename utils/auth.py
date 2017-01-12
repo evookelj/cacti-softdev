ca375799ed4f4d98f73ecec7e5b4d7a18e03d453
ChanYvonne <ychan1352@gmail.com>
2017-01-12 13:05:05 -0500

    move code and updated devlog

--- utils/auth.py
+++ utils/auth.py
@@ -3,39 +3,6 @@
 from hashlib import sha1
 from sqlite3 import connect
 from os import urandom
-import oauth2 as oauth
-import json#, twitter
-
-t = open("tw.txt","r")
-keys = t.read().split("\n")
-print keys
-CONSUMER_KEY = keys[0]
-CONSUMER_SECRET = keys[1]
-ACCESS_KEY = keys[2]
-ACCESS_SECRET = keys[3]
-t.close()
-
-consumer = oauth.Consumer(key=CONSUMER_KEY, secret=CONSUMER_SECRET)
-access_token = oauth.Token(key=ACCESS_KEY, secret=ACCESS_SECRET)
-client = oauth.Client(consumer, access_token)
-
-timeline_endpoint = "https://api.twitter.com/1.1/search/tweets.json?q=%23trump&result_type=popular"
-
-def get():
-    response, data = client.request(timeline_endpoint)
-    
-    tweets = json.loads(data)
-    data = []
-    for tweet in tweets['statuses']:
-        data.append(
-            {
-                'text': tweet['text'],
-                'favoriteCount': tweet['favorite_count'],
-                'retweeted': tweet['retweeted'],
-                'created_at': tweet['created_at']
-            }
-        )
-    print tweet['created_at']
 
 f = "data/quench.db"
 db = connect(f)
--- utils/twitter.py
+++ utils/twitter.py
@@ -1,4 +1,6 @@
 import sqlite3, time
+import oauth2 as oauth
+import json
 
 database = sqlite3.connect('data/database.db')
 
@@ -15,3 +17,35 @@ def addTweet(id, user, tweet, time):
     if (not tweetExists(id, user)):
         c.execute("INSERT INTO tweets VALUES (" + id + ", " + user +", " + tweet + ", " + time + ")")
         database.commit()
+
+
+t = open("tw.txt","r")
+keys = t.read().split("\n")
+print keys
+CONSUMER_KEY = keys[0]
+CONSUMER_SECRET = keys[1]
+ACCESS_KEY = keys[2]
+ACCESS_SECRET = keys[3]
+t.close()
+
+consumer = oauth.Consumer(key=CONSUMER_KEY, secret=CONSUMER_SECRET)
+access_token = oauth.Token(key=ACCESS_KEY, secret=ACCESS_SECRET)
+client = oauth.Client(consumer, access_token)
+
+timeline_endpoint = "https://api.twitter.com/1.1/search/tweets.json?q=%23trump&result_type=popular"
+
+def get():
+    response, data = client.request(timeline_endpoint)
+    
+    tweets = json.loads(data)
+    data = []
+    for tweet in tweets['statuses']:
+        data.append(
+            {
+                'text': tweet['text'],
+                'favoriteCount': tweet['favorite_count'],
+                'retweeted': tweet['retweeted'],
+                'created_at': tweet['created_at']
+            }
+        )
+    print tweet['created_at']
