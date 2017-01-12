820a908a8ad7defa689e7ba815dcdfbee2ea073b
ChanYvonne <ychan1352@gmail.com>
2017-01-12 10:57:51 -0500

    worked on implementing OAuth ugh

--- utils/auth.py
+++ utils/auth.py
@@ -3,6 +3,28 @@
 from hashlib import sha1
 from sqlite3 import connect
 from os import urandom
+import oauth2 as oauth
+import json, twitter
+
+t = open("api.txt","r")
+keys = t.read().split("\n")
+print keys
+CONSUMER_KEY = keys[0]
+CONSUMER_SECRET = keys[1]
+ACCESS_KEY = keys[2]
+ACCESS_SECRET = keys[3]
+twitter.close()
+
+consumer = oauth.Consumer(key=CONSUMER_KEY, secret=CONSUMER_SECRET)
+access_token = oauth.Token(key=ACCESS_KEY, secret=ACCESS_SECRET)
+client = oauth.Client(consumer, access_token)
+
+timeline_endpoint = "https://api.twitter.com/1.1/statuses/home_timeline.json"
+response, data = client.request(timeline_endpoint)
+
+tweets = json.loads(data)
+for tweet in tweets:
+    print tweet['text']
 
 f = "data/quench.db"
 db = connect(f)
--- utils/twitter.py
+++ utils/twitter.py
@@ -1,7 +1,17 @@
-import json
+import sqlite3, time
 
-twitter=open("api.txt","r")
-tw_key=twitter.read().strip
-twitter.close()
+database = sqlite3.connect('data/database.db')
 
+def tweetExists(id, user):
+    c = database.cursor()
+    b = c.execute('SELECT id FROM ' + user)
+    for chirp in b:
+        if chirp[0] == id:
+            return True
+    return False
 
+def addTweet(id, user, tweet, time):
+    c = database.cursor()
+    if (not tweetExists(id, user)):
+        c.execute("INSERT INTO tweets VALUES (" + id + ", " + user +", " + tweet + ", " + time + ")")
+        database.commit()
