1706960b21611e9bf8865447b0774c6b28d48dbe
evookelj <evukelj@stuy.edu>
2017-01-12 19:28:19 -0500

    UPdate devlog

--- utils/quench.py
+++ utils/quench.py
@@ -4,12 +4,10 @@ import textprocess, twitter
 def calcTime(tweet, hasImage):
     fromTweet = list(set(textprocess.tag(tweet)) | set(textprocess.phrase(tweet)))
     for keyword in tweet:
-        print keyword
         if " " not in keyword:
             twitter.addSearchTerm(keyword)
             datas = twitter.get()
             for data in datas:
-                #print data['text']
-                print "\n"
+                print data['text']
 
 calcTime("Donald Trump will never be my president", False);
