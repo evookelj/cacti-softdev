dc7a639845ff8bafae1a9aa92a386b3c549c9c43
Wilson Berkow <wilsonberkow@gmail.com>
2017-01-12 20:10:04 -0500

    Prevent 'Method Not Allowed' after login/register
    Create placeholder responses.

--- app.py
+++ app.py
@@ -2,8 +2,16 @@ from flask import Flask, session, request, url_for, redirect, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def home():
+    if request.method == "POST":
+        # TODO: Actually login/register
+        if 'form' in request.form:
+            return 'Invalid request.'
+        elif request.form['type'] == 'login':
+            return 'Login functionality <em>coming soon!</em>'
+        elif request.form['type'] == 'register':
+            return 'Register functionality <em>coming soon!</em>'
     return render_template('welcome.html')
 
 if __name__ == '__main__':
