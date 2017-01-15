b907012f67567c41be1d3761d2bf55e7140b407b
evookelj <evukelj@stuy.edu>
2017-01-15 11:27:45 -0500

    Add auth method to app

--- app.py
+++ app.py
@@ -14,6 +14,25 @@ def home():
             return 'Register functionality <em>coming soon!</em>'
     return render_template('welcome.html')
 
+@app.route("/authenticate/", methods=['POST'])
+def authenticate():
+    print request.form.values()
+    pw = request.form["password"]
+    un = request.form["user"]
+    em = request.form["email"]
+    tp = request.form["account"]#login vs. register
+    
+    if tp == "Register":
+        regRet = users.register(un,em,pw)#returns an error/success message
+        #return render_template('login.html', message = regRet)
+        
+    if tp == "Login":
+        text = users.login(un,em,pw)#error message
+        if text == "":#if no error message, succesful go back home
+            session["username"] = un
+            return redirect(url_for('home'))
+        #return render_template('login.html', message = text)
+
 if __name__ == '__main__':
     app.debug = True
     app.run()
