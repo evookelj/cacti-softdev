9b1ef00358a465ce736dbf9a35cfc925ad7fb154
ChanYvonne <ychan1352@gmail.com>
2017-01-18 14:16:38 -0500

    updated devlog and removed stuff

--- utils/twitter.py
+++ utils/twitter.py
@@ -69,18 +69,3 @@ if (__name__ == "__main__"):
     addSearchTerm("puppy");
     get()
 
-#database = sqlite3.connect('data/database.db')
-
-def tweetExists(id, user):
-    c = database.cursor()
-    b = c.execute('SELECT id FROM ' + user)
-    for chirp in b:
-        if chirp[0] == id:
-            return True
-    return False
-
-def addTweet(id, user, tweet, time):
-    c = database.cursor()
-    if (not tweetExists(id, user)):
-        c.execute("INSERT INTO tweets VALUES (" + id + ", " + user +", " + tweet + ", " + time + ")")
-        database.commit()
