f63d0a5357e4df2ef7ea632cf732ac6507fb676a
ayu7 <ayu7@stuy.edu>
2017-01-25 01:19:27 -0500

    minor edit

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, session, request, url_for, redirect, render_template
-from utils import auth, quench, twitter, util
+from utils import auth, quench, util
 
 app = Flask(__name__)
 app.secret_key = "deal with this later"
@@ -49,8 +49,12 @@ def callback():
     access = auth.getAccessToken(new_token, verifier)
     user = session["username"]
     resp = auth.update(access, user)
-    
-    return render_template("dashboard.html", username=user, message = resp) 
+
+    return render_template("dashboard.html", username=user, message = resp)
+
+@app.route("/tweeter/", methods=['POST'])
+def tweetForMe():
+    return render_template("dashboard.html", username=session["username"])
 
 @app.route("/logout/", methods=['POST'])
 def logout():
@@ -59,26 +63,15 @@ def logout():
 
 @app.route("/tweet/", methods=['POST'])
 def tweet():
+    if not 'tweet' in request.form:
+        return render_template("dashboard.html",username=session["username"], message = "Please write a tweet!")
     ui=request.form['tweet']
-    if len(ui)>140 or len(ui) == 0:
-            return render_template("dashboard.html", username=session["username"], message="Please enter a potential tweet that fits within the 140 character limit")
-
-    if "quench" in request.form:
-        results=quench.quench(session["username"],ui, False)
-        opt = results[0]
-        data = results[1]
-        return render_template("results.html", message=ui, time=str(opt[0])+":"+str(opt[1]), tweets=data)
-    
-    if "tweeter" in request.form:
-        if quench.exists(session["username"], ui):
-            return render_template("dashboard.html", username=session["username"], message = "You already tweeted that!")
-        if not auth.updated(session["username"]):
-            return render_template("dashboard.html", username=session["username"], message = "Please authenticate you account first!")
-    
-        resp = twitter.update_tweet(ui, session["username"])
-        return render_template("dashboard.html",
-                               username=session["username"],
-                               message = resp)
+    if len(ui)>140:
+        return render_template("dashboard.html", username=session["username"], message="Please enter a potential tweet that fits within the 140 character limit")
+    results=quench.quench(session["username"],ui, False)
+    opt = results[0]
+    data = results[1]
+    return render_template("results.html", message=ui, time=util.fmtTime(opt), tweets=data)
 
 @app.route("/about/")
 def about():
