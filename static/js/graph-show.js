47f40ac036e581537330a0f6c8ba8ccf979125c6
Wilson Berkow <wilsonberkow@gmail.com>
2017-01-25 08:52:14 -0500

    Add error handling for JSON parsing in graph-show.js

--- static/js/graph-show.js
+++ static/js/graph-show.js
@@ -7,7 +7,13 @@
     ctx.translate(0.5, 0.5);
 
     var graphContainer = document.getElementById("graph");
-    var graphData = JSON.parse(graphContainer.getAttribute("data-tweets"));
+    var graphData = [];
+    try {
+        graphData = JSON.parse(graphContainer.getAttribute("data-tweets"));
+    } catch (e) {
+        console.error(e);
+        console.error("PARSING data-tweets FAILED");
+    }
 
     Graphing.drawScatterplot(ctx, 30, 10, 400, 200, Graphing.sortDatapoints(graphData));
 }());
