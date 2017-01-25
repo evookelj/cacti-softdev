77fd9e3bb1085faeae234e012b41a01925ee28a8
Wilson Berkow <wilsonberkow@gmail.com>
2017-01-25 12:57:01 -0500

    Fix fmtTime to print 12:XXPM as 12:XXPM rather than 0:XXPM

--- utils/util.py
+++ utils/util.py
@@ -3,5 +3,7 @@ def fmtTime(hrmin):
         return "12:%02d AM" % hrmin[1]
     if hrmin[0] < 12:
         return "%d:%02d AM" % (hrmin[0], hrmin[1])
+    if hrmin[0] == 12:
+        return "12:%02d PM" % hrmin[1]
     return "%d:%02d PM" % (hrmin[0] - 12, hrmin[1])
 
